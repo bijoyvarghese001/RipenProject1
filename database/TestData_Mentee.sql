abc260c2fc16748e28c31695427f8f45b3dae06e
bijoyvarghese <bijoysbijoys123@gmail.com>
2022-05-20 15:05:19 -0600

    Test Data for Mentor and Mentee.

--- database/TestData_Mentee.sql
+++ database/TestData_Mentee.sql
@@ -42,5 +42,67 @@ INSERT INTO EducationalInfo VALUES('1017', 'Masters', 'Software Engineering', 'B
 INSERT INTO EducationalInfo VALUES('1018', 'Diploma', 'Business Information Systems', NULL, NULL)
 
 
-SELECT * FROM PersonalInfo
-SELECT * FROM EducationalInfo
+--Inserting Interests test data
+INSERT INTO Interests VALUES('1001','Project & Portfolio Management, Financial Management, Strategy & Governance, People & Resources, Data & Business Intelligence, Security & Risk','support with advancing expertise, leading teams within Acceptance/Acquiring/ Non-Traditional/Emerging Payments, IT/Data & Analytics, Finance, Business Transformation and Digital Strategy','Leadership in Disruptive Times, Strategic Decision Making & Driving Execution, Information Systems and e-Business Management, Building Well-Engineered & Customer-Centered Solutions Classified as Internal use only, Understanding the new Digital Consumer, Leading Virtual Teams, Advanced Emerging Payment Technologies, Advanced Knowledge of Banking Operations');
+INSERT INTO Interests VALUES('1002','People & Resources, Strategy & Governance, Service Planning & Architecture, Infrastructure & Operations','learning Crypto Development','new Skills that fit best in new Canada');
+INSERT INTO Interests VALUES('1003','Applications, Security & Risk, Data & Business Intelligence','a seasoned software professional looking forward to migrating/settling down in Saskatchewan soon. Like to expand network by sharing knowledge and gaining insight into local work culture','Cloud Technologies, Virtualization');
+INSERT INTO Interests VALUES('1004','Data & Business Intelligence, Infrastructure & Operations, Applications, Security & Risk, Service Planning & Architecture','working as a Business Analyst or System Design and Analyst','expand experience on Infrastructure Analysis and Data Analysis skills');
+INSERT INTO Interests VALUES('1005','Application and Data Business Intelligence','explore the front end technologies','Problem Solving, Communication, organzing task');
+INSERT INTO Interests VALUES('1006','Backend, Java development','getting to know people/improving English would be first priority','Finding IT related job skills');
+INSERT INTO Interests VALUES('1007','Data & Business Intelligence, Security & Risk, Applications','web development or to become a data scientist','develop more knowledge in Data management and Security field, to develop Website development skill such as JS, Node and Angular');
+INSERT INTO Interests VALUES('1008','Management, data base intelligence, Service Planning & Architecture, Security & Risk, Applications, Project & Portfolio Management, Learning Advanced Project management skills, Business Intelligence, Low code/No code technologies, IT security practices, leadership skills','Meeting new people and learn variety of cultures','Leadership skills, Presentation and Negotiation skills, Business Intelligence Low Code/No Code platforms Learn advanced/future technologies');
+INSERT INTO Interests VALUES('1009','Data & Business Intelligence, Applications, Project & Portfolio Management, Service Planning & Architecture','Developing Web Scraping applications, natural language processing applications, Learning and exploring new technology trends, Start working and contributing to open source projects','Utilizing cloud computing resources (Azure, AWS, Google Cloud) for data science applications, Mobile application development (Android and IOS platforms). been almost 10 years since I last worked on a mobile application (Symbian OS)');
+INSERT INTO Interests VALUES('1010','Data & Business Intelligence, Financial management, Service planning & architecture','Azure, DOT NET, Technical Leader','Blazor, Leadership');
+INSERT INTO Interests VALUES('1011','Programming, Software Engineering','Technology, Programming, Data Science, Artificial Intelligence','Standards expected to work in Saskatchewan, Certifications expected to work in Saskatchewan');
+INSERT INTO Interests VALUES('1012','Application, Project & Portfolio Management, Data & Business Intelligence, Service Planning & Architecture, Financial Management','Software Development, Algorithms, DevOps','Azure, DevOps');
+INSERT INTO Interests VALUES('1013','Application, Data & Business Intelligence, Infrastructure & Operations','involved in web development, cloud deployment, and server administration and found these areas interesting in career','develop on web & application development, cloud deployment and data analytics');
+INSERT INTO Interests VALUES('1014','Infrastructure and Operations, Security & Risk, Applications, Data and Business Intelligence, Strategy and Governance, Financial Management, People and Resources, Service Planning and Architecture, Project & Portfolio Management, Enterprise (Solutions) Architecture & Business Development, Development Operations (DevOps),Automation, NLP & Machine Learning, Cloud Computing & Infrastructure, Cybersecurity, Continuous learning – self driven education','Reading, Outdoor Sports & Recreation, Travelling, Cooking and Baking','Expand skills regarding Networking with industry professionals, Software development best practices, Communication and Leadership');
+INSERT INTO Interests VALUES('1015','Data & Business Intelligence, Applications, Security & Risk,Strategy & Governance, Service Planning & Architecture, Financial Management, Project & Portfolio Management, People & Resources','exploring about Data & Business Intelligence developer, enhance soft skills','Automations, Data engineering, Artificial Intelligence');
+INSERT INTO Interests VALUES('1016','Security & Risk, Data & Business Intelligence','Cloud Solutions and Services','Data Analysis, Intelligence and Visualization');
+INSERT INTO Interests VALUES('1017','Applications – Software Testing, Business Analysis, Product Owner, Project & Portfolio Management – Scrum Master','software testing, business analysis in building software application/services in e-commerce and Retail domain, Personal Fitness and self care, yoga and aerobic exercises','Requirement definition and management, Change Management, Client Services Management, Stakeholder Relationship management');
+INSERT INTO Interests VALUES('1018','Web design and Development, Digital Marketing,HTML5 (JQuery, CSS3), ASP.NET, JavaScript and Digital marketing','To get more exposure in the IT industry of Canada',null);
+
+
+--Inserting Goals test data
+INSERT INTO Goals VALUES ('1001','To lead and drive growth within the Technology space as a Business Director');
+INSERT INTO Goals VALUES ('1002','Short goal will be to start IT career in Canada in best possible way and In long term to attain a Management IT Position');
+INSERT INTO Goals VALUES ('1003','A seasoned software professional and looking forward to migrating/settling down in Saskatchewan soon. Like to expand my network by sharing knowledge and gaining insight into local work culture');
+INSERT INTO Goals VALUES ('1004','Become a Database Administrator or System Analyst');
+INSERT INTO Goals VALUES ('1005','front end developer');
+INSERT INTO Goals VALUES ('1006','find an entry level job related IT in Saskatoon in one year');
+INSERT INTO Goals VALUES ('1007','be a web development or to become a data scientist');
+INSERT INTO Goals VALUES ('1008','Find better opportunities. Establish as a technology leader in the Industry in the next five years');
+INSERT INTO Goals VALUES ('1009','Currently I am in the middle of second career shift going back to work in the industry as software engineer or developer.  I would like to establish myself back.  I would like to have my own development firm in 5 to 10 years');
+INSERT INTO Goals VALUES ('1010','Find a good job in SK');
+INSERT INTO Goals VALUES ('1011','I am willing to relocate to Saskatchewan and work as software engineer (programmer)');
+INSERT INTO Goals VALUES ('1012','To be a DevOps expert');
+INSERT INTO Goals VALUES ('1013','I would love to acquire skills in Data analytics and build a career in business intelligence');
+INSERT INTO Goals VALUES ('1014','Become an experienced professional in my field, guiding businesses towards accomplishing their strategic objectives with purpose-built software solutions; To be able to mentor others and pass on knowledge for future generations; Build a professional network in Saskatchewan and position the province to incubate home-grown tech talent');
+INSERT INTO Goals VALUES ('1015','"To work as a Business intelligence developer for innovating the business. By developing efficient solutions based upon the business needs');
+INSERT INTO Goals VALUES ('1016','Cloud based Data Science Expert');
+INSERT INTO Goals VALUES ('1017','To specialise in client engagement and requirement analysis area to help deliver and manage complex projects for the organization');
+INSERT INTO Goals VALUES ('1018','complete my degree in Business Information Systems with good grades and get a job in renowned IT firm or company that provides me enough opportunity and exposure to develop my professional skills');
+
+--Inserting Preference test data
+INSERT INTO Preference VALUES ('1001','I am currently the Head, Digital Banking in Stanbic IBTC Bank, Nigeria and would appreciate a mentor who has interfaced with different aspects of a driving a successful business and who has great coaching and mentoring skills. Experience working with Women and new professionals migrating to a new environment would be a huge benefit for me');
+INSERT INTO Preference VALUES ('1002','Would like me to guide in proper direction as I will be moving to new Country and culture');
+INSERT INTO Preference VALUES ('1007','I wish to have a mentor who experiencing in web development or data management');
+INSERT INTO Preference VALUES ('1008','Anyone who is comfortable with communicating people from different cultures and personalities');
+INSERT INTO Preference VALUES ('1009','I would recommend and prefer online meetings at the moment');
+INSERT INTO Preference VALUES ('1010','Mentor having more experience to me so he/she can guide better');
+INSERT INTO Preference VALUES ('1011','I would prefer someone working as software engineer (programmer/developer) in Saskatchewan who can guide me the way to get job there.  Once I relocated');
+INSERT INTO Preference VALUES ('1015','I would prefer to be matched with someone who Is working in the field of Data & Business Intelligence');
+INSERT INTO Preference VALUES ('1017','Any Business Analyst professional (preferably in E-commerce/Retail domain) Or a Senior Software Testing professional');
+INSERT INTO Preference VALUES ('1018','The one with website design or digital marketing background');
+
+
+
+SELECT * FROM PersonalInfo;
+SELECT * FROM EducationalInfo;
+SELECT * FROM Interests;
+SELECT * FROM Goals;
+SELECT * FROM Preference;
+
+
+
+
---
+++ database/TestData_Mentor.sql
@@ -0,0 +1,43 @@
+USE Mentor
+
+--Inserting PersonalInfo test data
+INSERT INTO PersonalInfo VALUES(1001,'Jaspreet Singh','Jaspreet.Singh@gmail.com','India','IT Manager','Software Architect','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1002,'Aarav Chauhan','aarav.chauhan@gmail.com','UK','Team Lead','Java Developer','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1003,'Rajesh Sharma','rajesh.sharma@gmail.com','USA','Lead Consultant','SAP Consultant','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1004,'Praveen wankde','praveen.wankde@gmail.com','Canada','Lead Developer','Full Stack Developer','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1005,'Juan Pablo','juan.pablo@gmail.com','Mexico','Manager','IT Architect','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1006,'Kevin Peterson','kevin.peterson@gmail.com','Germany','Software Professional','Technology Specialist','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1007,'Tony Williams','tony.williams@hotmail.com','Nigeria','IT Professional','Web Developer','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1008,'Seema Desouza','	seema.desouza@yahoo.com','SriLanka','Network Professional','Network Admin','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1009,'Aisyah Nur','nur.aisya@gmail.com','Malaysia','IT Administrator','Network Engineer','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1010,'Gerry Preneur','gerry.preneur@gmail.com','France','Software Professional','Software Engineer','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1011,'Prince Charles','price.charles@hotmail.com','South Africa','Analyst','IT Analyst','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1012,'David Thomson','david.thomson@yahoo.com','Italy','Business Manager','Business Consultant','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1013,'Rebecca Jose','jose.rebecca@gmail.com','Mexico','Security Professional','Security Analyst','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1014,'Rick Johnson','rick.johnson@gmail.com','Argentia','Quality Analyst','QA Lead','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1015,'Alasandro Fernandez','Alasandro.fernandez6@gmail.com','Peru','Project Management','Scrum Master','2022-03-04')
+INSERT INTO PersonalInfo VALUES(1016,'Robin Joseph','Robin.joseph@gmail.com','Kenya','Director','IT Ops','2022-03-04')
+
+
+-- Inserting Education Info test data
+INSERT INTO EducationalInfo VALUES('1001', 'Ph.D', NULL, NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1002', 'Masters', 'Computer Applications', NULL, 'India')
+INSERT INTO EducationalInfo VALUES('1003', 'PostGrad', 'Computer Applications', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1004', 'Masters', 'Computer Applications', NULL, 'India')
+INSERT INTO EducationalInfo VALUES('1005', 'Masters', 'Business Administration', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1006', 'Bachelors', 'Computer Science', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1007', 'Masters', 'Information Technology', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1008', 'Masters', 'Computer Application', NULL, 'India')
+INSERT INTO EducationalInfo VALUES('1009', 'Masters', 'Computer Application',NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1010', 'Bachelors', 'Mechanical Engineering', NULL, 'India')
+INSERT INTO EducationalInfo VALUES('1011', 'Masters','Information Technology', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1012', 'Bachelors','Computer Science', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1013', 'Bachelors', 'Computer Science', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1014', 'Masters', 'Project Management', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1015', 'Bachelors', 'Computer Science', NULL, NULL)
+INSERT INTO EducationalInfo VALUES('1016', 'Bachelors', 'Computer Application', NULL, NULL)
+
+
+
+SELECT * FROM PersonalInfo
+SELECT * FROM EducationalInfo
